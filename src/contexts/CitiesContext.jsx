6ff5c874136fa3c0feeda195286acb4d3e719feb
Linn Johansen <linn.es.johansen@gmail.com>
2024-11-22 18:35:30 -0800

    Map step 2

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,22 +1,23 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 
 const BASE_URL = 'http://localhost:8000';
+
 const CitiesContext = createContext();
 
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
-    setIsLoading(true);
-
     async function fetchCities() {
+      setIsLoading(true);
       try {
         const res = await fetch(`${BASE_URL}/cities`);
         const data = await res.json();
 
         setCities(data);
-      } catch (err) {
+      } catch {
         alert('There was an error loading data...');
       } finally {
         setIsLoading(false);
@@ -25,8 +26,22 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+
+      setCurrentCity(data);
+    } catch {
+      alert('There was an error loading data...');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <CitiesContext.Provider value={{ cities, isLoading }}>
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
       {children}
     </CitiesContext.Provider>
   );

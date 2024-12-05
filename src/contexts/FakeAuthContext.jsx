c463d3ed7e5ff76846ae514c856c94b6d945d6cc
Linn Johansen <linn.es.johansen@gmail.com>
2024-12-05 13:12:16 -0800

    Implementing login system, pt 2

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -56,6 +56,8 @@ function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error('This context was used outside the AuthProvider');
+
+  return context;
 }
 
 export { AuthProvider, useAuth };
